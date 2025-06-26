994d14affe74cdf9ac90a0a703ea104330af0248
webpjm <13012264890@163.com>
2025-06-26 17:31:53 +0800

    优化寻找小程序的按钮

--- public/DyProject/代码/临时测试.js
+++ public/DyProject/代码/临时测试.js
@@ -1,13 +1,14 @@
 
 // 调用 OCR 功能识别中文，并将结果输出
 runTime.Import('initData.js')
-// runTime.Import('douyinAd.js')
+runTime.Import('douyinAd.js')
 runTime.Import('tool.js')
 //  runTime.Import('快手/ksAdTool.js')
 // runTime.Import('快手/kuaishouAd.js')
 // runTime.Import('快手/ksInit.js')
 
-autoUtils.clickGetText('首页')
+
+// autoUtils.clickGetText('首页')
 // console.log(parseInt(500/10))
 // kaishouAd.swipeIndexAppListForAd({appName:'洛雪壁纸'}, 1)
 // kaishouAd.lookAd('洛雪壁纸')
