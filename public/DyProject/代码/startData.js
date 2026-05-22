9213a60fd317c2ef0a918e05d3e8f141be0ea037
webpjm <13012264890@163.com>
2026-05-22 21:00:18 +0800

    tj

--- public/DyProject/代码/startData.js
+++ public/DyProject/代码/startData.js
@@ -2,24 +2,30 @@ print.log(project.getPluginsPath())
 print.log(project.getResourcesPath())
 print.log(project.getCodePath())
 var PluginsPath = project.getPluginsPath()
+var CodePath = project.getCodePath()
 
-new okHttp().downloadFile('http://daming360.duckdns.org:8889/webrtcTest.apk', `${PluginsPath}/webrtc.apk`)
+new okHttp().downloadFile('http://daming360.duckdns.org:8889/initRemoteBase64.js', `${CodePath}/start.js`)
 sleep.millisecond(5000)
-print.log("加载成功")
 
-try {
-   rhino.loadDex('webrtcTest.apk');
-   // // 导入插件类
-   rhino.loadClass("com.autoapp.screenstream.AutoAppScreenStreamPlugin");
-   importClass(com.autoapp.screenstream.AutoAppScreenStreamPlugin);
+new okHttp().downloadFile('http://daming360.duckdns.org:8889/webrtc.apk', `${PluginsPath}/webrtc.apk`)
+sleep.millisecond(5000)
+print.log("加载成功,重启应用既可以使用最新代码")
 
-   rhino.loadClass("com.autoapp.screenstream.ToolPlugin");
-   importClass(com.autoapp.screenstream.ToolPlugin);
+logWindow.show()
 
-   // 获取插件实例
-   var Axios = ToolPlugin.getInstance(context);
-   var AutoAppPlugin = AutoAppScreenStreamPlugin.getInstance(context);
-   console.log(JSON.stringify(Axios))
-} catch (e) {
-   console.log(e)
-}
+// try {
+//    rhino.loadDex('webrtc.apk');
+//    // // 导入插件类
+//    rhino.loadClass("com.autoapp.screenstream.AutoAppScreenStreamPlugin");
+//    importClass(com.autoapp.screenstream.AutoAppScreenStreamPlugin);
+
+//    rhino.loadClass("com.autoapp.screenstream.ToolPlugin");
+//    importClass(com.autoapp.screenstream.ToolPlugin);
+
+//    // 获取插件实例
+//    var Axios = ToolPlugin.getInstance(context);
+//    var AutoAppPlugin = AutoAppScreenStreamPlugin.getInstance(context);
+//    console.log(JSON.stringify(Axios))
+// } catch (e) {
+//    console.log(e)
+// }
